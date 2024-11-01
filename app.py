32a8a0bc10b6edc58f747f3856c229d415200fe8
whywhoyyboy <ilia8394@mail.ru>
2024-11-01 04:14:06 +0300

    Initial comit

---
+++ app.py
@@ -0,0 +1,20 @@
+import os
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    return render_template("global_page.html")
+
+@app.route('/about')
+def about():
+    return render_template("about.html")
+
+@app.route('/menu')
+def menu():
+    return  render_template("menu.html")
+
+if __name__ == '__main__':
+    app.run(debug=True,host='0.0.0.0',port=int(os.environ.get("PORT",5000)))
